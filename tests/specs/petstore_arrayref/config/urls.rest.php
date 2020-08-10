dd676c1ced88f91204464bd7dd23d33196eb17fa
insolita <webmaster100500@ya.ru>
2020-08-10 15:24:12 +0800

    fix url fixtures for #f6afc494

--- tests/specs/petstore_arrayref/config/urls.rest.php
+++ tests/specs/petstore_arrayref/config/urls.rest.php
@@ -4,12 +4,12 @@
  *
  * This file is auto generated.
  */
-return array (
-  'GET pets' => 'pet/index',
-  'POST pets' => 'pet/create',
-  'GET pets/<petId>' => 'pet/view',
-  'DELETE pets/<petId>' => 'pet/delete',
-  'PATCH pets/<petId>' => 'pet/update',
-  'pets' => 'pet/options',
-  'pets/<petId>' => 'pet/options',
-);
+return [
+    'GET pets' => 'pet/index',
+    'POST pets' => 'pet/create',
+    'GET pets/<petId>' => 'pet/view',
+    'DELETE pets/<petId>' => 'pet/delete',
+    'PATCH pets/<petId>' => 'pet/update',
+    'pets' => 'pet/options',
+    'pets/<petId>' => 'pet/options',
+];
