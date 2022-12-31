09a5fb85c52fee411076c8b0b20f84e2c2fc08e5
Sohel Ahmed Mesaniya <sam.wankaner@gmail.com>
2022-12-31 21:09:39 +0530

    Add a test for float issue in PgSQL

--- tests/unit/IssueFixTest.php
+++ tests/unit/IssueFixTest.php
@@ -9,6 +9,7 @@ use yii\helpers\FileHelper;
 // This class contains tests for various issues present at GitHub
 class IssueFixTest extends DbTestCase
 {
+    // TODO WIP resume from here
     // fix https://github.com/cebe/yii2-openapi/issues/107
     // 107_no_syntax_error
     public function testMigrationsAreNotGeneratedWithSyntaxError()
@@ -46,6 +47,35 @@ class IssueFixTest extends DbTestCase
         Yii::$app->db->createCommand()->createTable('{{%fruits}}', [
             'id' => 'pk',
             'name' => 'varchar(255)',
+        ])->execute();
+    }
+
+    public function testFloatIssue()
+    {
+        // test no migrations are generaeted
+        $this->changeDbToPgsql();
+        $this->deleteTablesForFloatIssue();
+        $this->createTableForFloatIssue();
+        $testFile = Yii::getAlias("@specs/issue_fix/float_issue/float_issue.php");
+        $this->runGenerator($testFile, 'pgsql');
+        $this->expectException(\yii\base\InvalidArgumentException::class);
+        FileHelper::findDirectories(Yii::getAlias('@app').'/migration');
+        FileHelper::findDirectories(Yii::getAlias('@app').'/migrations');
+        FileHelper::findDirectories(Yii::getAlias('@app').'/migrations_mysql_db');
+        FileHelper::findDirectories(Yii::getAlias('@app').'/migrations_maria_db');
+        FileHelper::findDirectories(Yii::getAlias('@app').'/migrations_pgsql_db');
+    }
+
+    private function deleteTablesForFloatIssue()
+    {
+        Yii::$app->db->createCommand('DROP TABLE IF EXISTS {{%fruits}}')->execute();
+    }
+
+    private function createTableForFloatIssue()
+    {
+        Yii::$app->db->createCommand()->createTable('{{%fruits}}', [
+            'id' => 'pk',
+            'vat_percent' => 'float default 0',
         ])->execute();
     }
 }
